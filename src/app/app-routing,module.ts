20fc26abd839925a3c795d767b1b6da570baa746
DANIELW-PC\Daniel <daniel.ward@hetras.com>
2018-02-12 09:49:12 +0100

    Changed target to es2017

--- src/app/app-routing,module.ts
+++ src/app/app-routing,module.ts
@@ -3,9 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './core/error-page/error-page.component';
 
 const routes: Routes = [
-  { path: 'error-page', component: ErrorPageComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: '**', redirectTo: 'error-page', pathMatch: 'full' }
+  { path: '**', redirectTo: 'error-page', pathMatch: 'full' },
+  { path: 'error-page', component: ErrorPageComponent }
 ];
 
 @NgModule({
